22fac2d2679ddafd76b42f9dfe464fa53ffb21f6
Rydanyaina <74042818+Rydanyainav8@users.noreply.github.com>
2024-09-23 10:52:28 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<!-- COMPOSITION API -->
+<script>
+import { ref } from 'vue';
+
+export default{
+  setup(){
+    const name = ref('John Doe');
+    const status = ref('active');
+    const tasks = ref(['Task one', 'Task two', 'Task three']);
+
+    const toggleStatus = () => {
+      if(status.value === 'active'){
+        status.value = 'pending';
+      }else if(status.value === 'pending'){
+        status.value = 'inactive';
+      }else{
+        status.value = 'active';
+      }
+    }
+    return {
+      name, 
+      status, 
+      tasks,
+      toggleStatus,
+    }
+  }
+};
+</script>
+
+<template>
+  <h1>Hello, my name is {{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is unactive</p>
+  <h3>Task list:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul> 
+  <!-- <button v-on:click="toggleStatus()">Change status</button> -->
+  <button @click="toggleStatus()">Change status</button>
+
+</template>
+
+<style scoped>
+
+</style>
