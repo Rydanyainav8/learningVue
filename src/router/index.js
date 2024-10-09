1fe8027b7bc1685893075e788c39a2511feeec25
Rydanyaina <74042818+Rydanyainav8@users.noreply.github.com>
2024-10-09 13:19:30 +0300

    Edit Feature

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import JobView from '@/views/JobView.vue';
 import NotFoundView from '@/views/NotFoundView.vue';
 import JobsView from '@/views/JobsView.vue';
 import AddJobView from '@/views/AddJobView.vue';
+import EditJobView from '@/views/EditJobView.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,6 +28,11 @@ const router = createRouter({
             path: '/job/add',
             name: 'add-job',
             component: AddJobView
+        },
+        {
+            path: '/job/edit/:id',
+            name: 'edit-job',
+            component: EditJobView
         },
         {
             path: '/:catchAll(.*)',
