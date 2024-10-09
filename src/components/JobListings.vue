6c869d1f8919904b19afa96867b3b0255fe3ab64
Rydanyaina <74042818+Rydanyainav8@users.noreply.github.com>
2024-10-09 10:01:35 +0300

    Add Job Listing&View

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,9 +1,10 @@
 <script setup>
-import jobData from '../../jobs.json';
-import { ref, defineProps } from 'vue';
+import { reactive, onMounted } from 'vue';
 import JobListing from './JobListing.vue';
 import { RouterLink } from 'vue-router';
-const jobs = ref(jobData);
+import axios from 'axios';
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
+// const jobs = ref(jobData);
 
 defineProps({
     limit: Number,
@@ -13,6 +14,22 @@ defineProps({
     }
 })
 // console.table(jobs.value);
+const state = reactive({
+    jobs: [],
+    isLoading: true,
+});
+
+onMounted(async () => {
+    try{
+        const response = await axios.get('/api/jobs');
+        state.jobs = response.data;
+    }catch(error){
+        console.error('Error fetching jobs data', error);
+    }finally{
+        state.isLoading = false;
+    }
+});
+
 </script>
 
 <template>
@@ -21,8 +38,13 @@ defineProps({
             <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
                 Browse Jobs
             </h2>
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" />
+            <!-- Show loading spinner while loading is true -->
+            <div v-if="state.isLoading" class="text-center text-gray-500 pu=y-6">
+                <PulseLoader />
+            </div>
+            <!-- Show job listing when done loading -->
+            <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job" />
             </div>
         </div>
     </section>
@@ -33,4 +55,5 @@ defineProps({
         class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
         >View All Jobs</RouterLink>
     </section>
+
 </template>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -22,7 +22,7 @@ const isActiveLink = (routePath) => {
                         <div class="flex space-x-2">
                             <RouterLink to="/"
                                 :class="[isActiveLink('/') ? 'bg-green-900' : 'hover:bg-gray-900 hover:text-white', 'text-white', 'px-3', 'py-2', 'rounded-md',]">Home</RouterLink>
-                                <RouterLink to="/list-jobs"
+                                <RouterLink to="/jobs"
                                 :class="[isActiveLink('/list-jobs') ? 'bg-green-900' : 'hover:bg-gray-900 hover:text-white', 'text-white', 'px-3', 'py-2', 'rounded-md',]">Jobs</RouterLink>
                                 <RouterLink to="/jobs/add"
                                 :class="[isActiveLink('/jobs/add') ? 'bg-green-900' : 'hover:bg-gray-900 hover:text-white', 'text-white', 'px-3', 'py-2', 'rounded-md',]">Add Jobs</RouterLink>
